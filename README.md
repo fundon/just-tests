e5cca82fccdeca8acdd87c90faf6538e99230eb2
Fangdun Cai <cfddream@gmail.com>
2017-03-18 16:39:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Just Tests
+---
+
+## Langs
+
+* [c](c)
+
+---
+
+> [fundon.me](https://fundon.me) &nbsp;&middot;&nbsp;
+> GitHub [@fundon](https://github.com/fundon) &nbsp;&middot;&nbsp;
+> Twitter [@_fundon](https://twitter.com/_fundon)
---
+++ c/point.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  int a[10] = {0,1,2,3,};
+
+  printf("a[1]:       %i\n", a[1]);
+  printf("*(a + 2):   %i\n", *(a + 2));
+
+  int *r = a;
+
+  r += 1;
+  printf("*r:         %i\n", *r);
+
+  r += 2;
+  printf("*r:         %i\n", *r);
+
+
+  unsigned char im[10] = "123456789";
+  unsigned char *n = im + 1;
+  unsigned char *p = n;
+
+  printf("im:         %s\n", im);
+  printf("n:          %s\n", n);
+  printf("p:          %s\n", p);
+
+  printf("*n:         %i\n", *n);
+  printf("*n:         %c\n", *n);
+
+  n += 2;
+  p = n;
+
+  printf("*p:         %i\n", *p);
+  printf("*p:         %c\n", *p);
+
+  return 0;
+}
