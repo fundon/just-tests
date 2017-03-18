2c9d319d7f18c1390d4b15392c731c44f33742a6
Fangdun Cai <cfddream@gmail.com>
2017-03-18 17:28:35 +0800

    c: update point.c

--- c/point.c
+++ c/point.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
   int a[10] = {0,1,2,3,};
@@ -18,31 +19,41 @@ int main(int argc, char **argv) {
 
   printf("\n");
 
+  int8_t lt[] = { 3, 4, 5, 6, 7, 8, 9, 2, 1, 0 };
+  int8_t *p = lt;
   unsigned char im[10] = "123456789";
   unsigned char *n = im + 1;
-  unsigned char *p = n;
+  unsigned char *i = n;
 
+  printf("lt:         %s\n", lt);
   printf("im:         %s\n", im);
   printf("n:          %s\n", n);
+  printf("i:          %s\n", i);
+
   printf("p:          %s\n", p);
+  printf("*p:         %i\n", *p);
 
   printf("\n");
 
   printf("*n:         %i\n", *n);
   printf("*n:         %c\n", *n);
-  printf("*p:         %i\n", *p);
-  printf("*p:         %c\n", *p);
+  printf("*i:         %i\n", *i);
+  printf("*i:         %c\n", *i);
 
   n += 2;
-  p = n;
+  i = n;
+  i +=-*p;
 
   printf("\n");
+
+  printf("p:          %s\n", p);
+  printf("*p:         %i\n", *p);
 
   printf("*n:         %i\n", *n);
   printf("*n:         %c\n", *n);
 
-  printf("*p:         %i\n", *p);
-  printf("*p:         %c\n", *p);
+  printf("*i:         %i\n", *i);
+  printf("*i:         %c\n", *i);
 
   return 0;
 }
