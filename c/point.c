9e25c1076aa08878f337ad76f669e938b6e682c5
Fangdun Cai <cfddream@gmail.com>
2017-03-18 17:01:29 +0800

    c: update point.c

--- c/point.c
+++ c/point.c
@@ -16,6 +16,8 @@ int main(int argc, char **argv) {
   printf("*r:         %i\n", *r);
 
 
+  printf("\n");
+
   unsigned char im[10] = "123456789";
   unsigned char *n = im + 1;
   unsigned char *p = n;
@@ -24,11 +26,20 @@ int main(int argc, char **argv) {
   printf("n:          %s\n", n);
   printf("p:          %s\n", p);
 
+  printf("\n");
+
   printf("*n:         %i\n", *n);
   printf("*n:         %c\n", *n);
+  printf("*p:         %i\n", *p);
+  printf("*p:         %c\n", *p);
 
   n += 2;
   p = n;
+
+  printf("\n");
+
+  printf("*n:         %i\n", *n);
+  printf("*n:         %c\n", *n);
 
   printf("*p:         %i\n", *p);
   printf("*p:         %c\n", *p);
